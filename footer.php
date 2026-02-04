ff97d851bf1b3d4e6c95eb34ac4b62ada7b3c9ec
Marc Heatley <marc.heatley@gmail.com>
2026-02-04 09:49:12 +0000

    moves footer socials to module

--- footer.php
+++ footer.php
@@ -10,26 +10,18 @@
 </div>
 
 <div class="siteFooter-wrapper">
-
-<div class="siteFooter-menus">
-
-	<?php get_template_part( 'template-parts/footer-nav' ); ?>
-	<ul class="socials">
-		<li><a href="https://bsky.app/profile/archiveswales.bsky.social">Bluesky</a></li>
-		<li><a href="https://www.instagram.com/archifaucymru.archiveswales/">Instagram</a></li>
-		<li><a href="https://www.facebook.com/ArchifauCymru.ArchivesWales">Facebook</a></li>
-	</ul>
-</div>
-	<footer id="footer" class="siteFooter">
-		<div class="siteFooter-logos">
-			<?php get_template_part( 'template-parts/logo' ); ?>
-			<?php get_template_part( 'template-parts/wg-logo' ); ?>
+	<div class="siteFooter-menus">
+		<?php get_template_part( 'template-parts/footer-nav' ); ?>
+		<?php get_template_part( 'template-parts/footer-socials' ); ?>
 		</div>
-		
-		<div class="source-org vcard copyright" role="contentinfo">
-
+			<footer id="footer" class="siteFooter">
+				<div class="siteFooter-logos">
+					<?php get_template_part( 'template-parts/logo' ); ?>
+					<?php get_template_part( 'template-parts/wg-logo' ); ?>
+				</div>
 			
-			<div class="copyright">
+				<div class="source-org vcard copyright" role="contentinfo">
+					<div class="copyright">
 				
 				
 				<?php
---
+++ template-parts/footer-socials.php
@@ -0,0 +1,5 @@
+	<ul class="socials">
+		<li><a href="https://bsky.app/profile/archiveswales.bsky.social"><?php get_template_part('svg-bluesky.php'); ?> Bluesky</a></li>
+		<li><a href="https://www.instagram.com/archifaucymru.archiveswales/"><?php get_template_part('svg-instagram.php'); ?> Instagram</a></li>
+		<li><a href="https://www.facebook.com/ArchifauCymru.ArchivesWales"><?php get_template_part('svg-facebook.php'); ?> Facebook</a></li>
+	</ul>
