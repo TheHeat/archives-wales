90834efea53ebc1a9ee5e04038a8cd6d7028da07
Marc Heatley <marc@properdesign.co.uk>
2026-02-04 10:27:30 +0000

    responsive footer

--- template-parts/footer-socials.php
+++ template-parts/footer-socials.php
@@ -1,5 +1,5 @@
 	<ul class="footerSocials">
-		<li><a href="https://bsky.app/profile/archiveswales.bsky.social"><span class="footerSocials-icon"><?php get_template_part('template-parts/svg-bluesky'); ?></span> Bluesky</a></li>
-		<li><a href="https://www.instagram.com/archifaucymru.archiveswales/"><span class="footerSocials-icon"><?php get_template_part('template-parts/svg-instagram'); ?></span> Instagram</a></li>
-		<li><a href="https://www.facebook.com/ArchifauCymru.ArchivesWales"><span class="footerSocials-icon"><?php get_template_part('template-parts/svg-facebook'); ?></span> Facebook</a></li>
+		<li><a href="https://bsky.app/profile/archiveswales.bsky.social"><span class="footerSocials-icon"><?php get_template_part('template-parts/svg-bluesky'); ?></span> <?php _e('Bluesky', 'acaw'); ?></a></li>
+		<li><a href="https://www.instagram.com/archifaucymru.archiveswales/"><span class="footerSocials-icon"><?php get_template_part('template-parts/svg-instagram'); ?></span> <?php _e('Instagram', 'acaw'); ?></a></li>
+		<li><a href="https://www.facebook.com/ArchifauCymru.ArchivesWales"><span class="footerSocials-icon"><?php get_template_part('template-parts/svg-facebook'); ?></span> <?php _e('Facebook', 'acaw'); ?></a></li>
 	</ul>
