fa5eab86950fa9b5fe03999df1f4a90b7561e82e
Marc Heatley <marc@properdesign.co.uk>
2026-02-04 10:01:40 +0000

    adds social icons to the footer menu

--- template-parts/footer-socials.php
+++ template-parts/footer-socials.php
@@ -1,5 +1,5 @@
-	<ul class="socials">
-		<li><a href="https://bsky.app/profile/archiveswales.bsky.social"><?php get_template_part('svg-bluesky.php'); ?> Bluesky</a></li>
-		<li><a href="https://www.instagram.com/archifaucymru.archiveswales/"><?php get_template_part('svg-instagram.php'); ?> Instagram</a></li>
-		<li><a href="https://www.facebook.com/ArchifauCymru.ArchivesWales"><?php get_template_part('svg-facebook.php'); ?> Facebook</a></li>
+	<ul class="footerSocials">
+		<li><a href="https://bsky.app/profile/archiveswales.bsky.social"><span class="footerSocials-icon"><?php get_template_part('template-parts/svg-bluesky'); ?></span> Bluesky</a></li>
+		<li><a href="https://www.instagram.com/archifaucymru.archiveswales/"><span class="footerSocials-icon"><?php get_template_part('template-parts/svg-instagram'); ?></span> Instagram</a></li>
+		<li><a href="https://www.facebook.com/ArchifauCymru.ArchivesWales"><span class="footerSocials-icon"><?php get_template_part('template-parts/svg-facebook'); ?></span> Facebook</a></li>
 	</ul>
