b810b5c3b91092fb625f89bd9ff6bf2b71f50036
Marc Heatley <marc@properdesign.co.uk>
2026-02-10 15:05:08 +0000

    adds a function to clean website addresses

--- inc/proper-strip-protocol.php
+++ inc/proper-strip-protocol.php
@@ -12,3 +12,18 @@ function proper_strip_protocol( $url ) {
 
 
 
+//
+// return just the domain and TLD from a given url //
+//
+function proper_clean_address( $url ) {
+	// Remove protocol and www
+	$host = preg_replace( '#^https?://#', '', $url );
+	$host = preg_replace( '#^www\.#', '', $host );
+	// Extract domain and TLD
+	$parts = explode('/', $host, 2);
+	$domain = $parts[0];
+	return $domain;
+}
+
+
+
