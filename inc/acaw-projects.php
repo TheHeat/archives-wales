12de3012369104d4d1ee5f7ee4ce21dfa8890f59
Marc Heatley <marc@properdesign.co.uk>
2026-03-11 16:17:56 +0000

    apply project filter ONLY to archive

--- inc/acaw-projects.php
+++ inc/acaw-projects.php
@@ -2,7 +2,7 @@
 
 function projects_query($query) {
 
-	if (! is_admin() && $query->is_main_query() && isset($query->query['post_type']) && $query->query['post_type']=='project') {
+	if (! is_admin() && $query->is_main_query() && is_post_type_archive('project') ) {
 		// Order projects by end_date meta key in descending order
 		// Exclude projects with an end_date and order by start_date
 		$query->set('meta_query', array(
