6921da81e62117fcabc1684c27021df426bf360b
Marc Heatley <marc@properdesign.co.uk>
2026-03-26 11:10:41 +0000

    map teaser title

--- template-parts/map-teaser.php
+++ template-parts/map-teaser.php
@@ -3,7 +3,7 @@
 
 <?php get_template_part('template-parts/map-with-query'); ?>
 	<div>
-		<h2><?php _e('There are Archives Wales members in every corner of the country', 'acaw'); ?></h2>
+		<h2><?php _e('There are Archive Services in every corner of the country', 'acaw'); ?></h2>
 		<p><?php _e('Explore the complete list of our members along with a map to help you find your nearest archive.', 'acaw'); ?></p>
 		<a class="button" href="<?php echo get_post_type_archive_link('organisation'); ?>"><?php _e('Meet our members', 'acaw'); ?></a>
 	</div>	
