5f62aa30e9ac8d06510cf43979be04fb828e2cf6
Carl Morris <carl@morris.cymru>
2026-03-29 23:28:13 +0100

    add text domain (to fix buy of Cy not showing)

--- template-parts/members-link.php
+++ template-parts/members-link.php
@@ -8,7 +8,7 @@ $link = get_post_type_archive_link('organisation');
 <?php get_template_part('template-parts/map-with-query');?>
 	<a class="membersLink" href="<?php echo $link?>">
 		<h2>
-			<?php _e('Our member repositories');?>
+			<?php _e('Our member repositories', 'acaw');?>
 		</h2>
 </a>
 </div>
