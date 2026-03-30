19e1972772e2515737553d7c96c6cd4e5a795424
Marc Heatley <marc.heatley@gmail.com>
2026-03-30 15:45:47 +0100

    map zooms to available pins

--- src/js/map/index.js
+++ src/js/map/index.js
@@ -7,11 +7,11 @@ const initMap = ({ id, markers }) => {
 	// console.log('Initializing map with markers:', markers);
 
 	if (mapElement) {
-		const map = L.map(mapElement).setView([52.5, -3.5], 7);
+		const map = L.map(mapElement);
 		map.attributionControl.setPrefix(false);
 
 		L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-			maxZoom: 19,
+
 			attribution: false,
 		}).addTo(map);
 
@@ -23,15 +23,31 @@ const initMap = ({ id, markers }) => {
 			popupAnchor: [0, -24],
 		});
 
+		const validMarkerPoints = [];
+
 		// loop markers and add to map
 		markers.forEach((marker) => {
 			const { title, url, lat, lng, label } = marker;
 			const popupContent = `<a href="${url}" target="_blank" rel="noopener">${title}</a><div>${label}</div>`;
+			const latitude = Number(lat);
+			const longitude = Number(lng);
 
-			if (lat && lng) {
-				L.marker([lat, lng], { icon: svgIcon }).addTo(map).bindPopup(popupContent);
+			if (Number.isFinite(latitude) && Number.isFinite(longitude)) {
+				const point = [latitude, longitude];
+				validMarkerPoints.push(point);
+				L.marker(point, { icon: svgIcon }).addTo(map).bindPopup(popupContent);
 			}
 		});
+
+		if (validMarkerPoints.length === 1) {
+			map.setView(validMarkerPoints[0], 12);
+		} else if (validMarkerPoints.length > 1) {
+			map.fitBounds(validMarkerPoints, {
+				padding: [24, 24],
+			});
+		} else {
+			map.setView([52.5, -3.5], 7);
+		}
 	}
 };
 
